0441007dbf2e258ce2b83b160e543e1d9a1d7751
Boluwatife Oduyemi <59292038+mowumib@users.noreply.github.com>
2021-03-04 12:29:05 +0100

    Hangman Game

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 const wordEl = document.getElementById("word");
 const WrongLettersEL = document.getElementById("wrong-letters");
 const playAgainBtn = document.getElementById("play-button");
